45e8eaee77bf4e456b1ba673d8c595d1a01746de
Jan Tache <j.andreitabs@gmail.com>
2018-02-04 02:25:36 -0800

    separated fill from outline for more accurate representation

--- shapelib/shapelib.go
+++ shapelib/shapelib.go
@@ -493,34 +493,50 @@ func (p Path)GetSubArray() PixelSubArray {
 	xStart := p.Points[0].X
 	yStart := p.Points[0].Y
 
+	// Fill separately from doing the outline - more accurate
+	if p.Filled {
+		for i := 0; i < len(p.Points) - 1; i++ {
+			// Get the iterator for pixels along a line between points.
+			nextPoint, vertDir := linePointsGen(p.Points[i], p.Points[i+1])
+
+			// Random crap that seems to work.
+			yPrev := p.Points[i].Y
+			if prevVertDir != vertDir && prevVertDir != 0 {
+				sub.flipAllRight(p.Points[i].X, yPrev)
+			}
+
+			prevVertDir = vertDir
+
+			// Fill in the pixels provided by the iterator
+			for x, y := nextPoint(); x != -1; x, y = nextPoint() {
+				if y != yPrev {
+					if p.Filled && vertDir != 0 &&
+					!(x == xStart && y == yStart) {
+						sub.flipAllRight(x, y)
+					}
+					yPrev = y
+				}
+			}
+		}
+	}
+
+	// Do the outline of the shape
 	for i := 0; i < len(p.Points) - 1; i++ {
 		// Get the iterator for pixels along a line between points.
 		nextPoint, vertDir := linePointsGen(p.Points[i], p.Points[i+1])
 
-		// Random crap that seems to work.
-		yPrev := p.Points[i].Y
-		if p.Filled && prevVertDir != vertDir && prevVertDir != 0 {
-			sub.flipAllRight(p.Points[i].X, yPrev)
-		}
-
 		prevVertDir = vertDir
+		yPrev := p.Points[i].Y
 
 		// Fill in the pixels provided by the iterator
 		for x, y := nextPoint(); x != -1; x, y = nextPoint() {
 			if y != yPrev {
-				if p.Filled && vertDir != 0 &&
-						!(x == xStart && y == yStart) {
-					sub.flipAllRight(x, y)
-				}
-
-				// Set after fill so bit doesn't get flipped
 				// This set is done to make sure that the pixels
 				// are continuous; such as in a 45 degree line.
 				sub.set(x, yPrev)
 				yPrev = y
 			}
 
-			// Set after potential fill - see above
 			sub.set(x, y)
 		}
 	}
