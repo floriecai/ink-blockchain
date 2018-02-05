8e4fee6e552177ffaca599b4d1ec3c733c837850
Ruimou Xu <i6y8@ugrad.cs.ubc.ca>
2018-02-05 10:41:28 -0800

    add skeleton for lib-miner-api

--- miner/miner.go
+++ miner/miner.go
@@ -32,7 +32,24 @@ func (lmi *Lib_Miner_Interface) OpenCanvas(hello *[]byte, response *lib_miner_st
 	return nil
 }
 
-func (lmi *Lib_Miner_Interface) GetInk(hello *[]byte, response *lib_miner_structs.InkResponse)(err error) {
+func (lmi *Lib_Miner_Interface) GetInk(hello *[]byte, response *lib_miner_structs.InkResponse) (err error) {
+	return nil
+}
+
+func (lmi *Lib_Miner_Interface) Draw(req *lib_miner_structs.DrawRequest, response *lib_miner_structs.DrawResponse) (err error) {
+	return nil
+}
+
+func (lmi *Lib_Miner_Interface) Delete(req *lib_miner_structs.DeleteRequest, response *lib_miner_structs.InkResponse) (err error) {
+	return nil
+}
+/* TODO
+func (lmi *Lib_Miner_Interface) GetBlockChain(hello *[]byte, response *[]Block) (err error) {
+	return nil
+}
+*/
+
+func (lmi *Lib_Miner_Interface) GetGenesisBlock(hello *[]byte, response *string) (err error) {
 	return nil
 }
 /*******************************
