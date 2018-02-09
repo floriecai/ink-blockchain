9f45b8ae95f264114b35b6e00477f73e333e90ac
jtache <j.andreitabs@gmail.com>
2018-02-09 12:55:42 -0800

    added simple build testing script for project

---
+++ test-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Use to make sure that everything compiles fine.
+# Does not check proj1-tester right now.
+
+go test ./*.go
+go test ./blockartlib/*.go
+go test ./libminer/*.go
+go test ./miner/*.go
+go test ./minerserver/*.go
+go test ./shapelib/*.go
+go test ./utils/*.go
