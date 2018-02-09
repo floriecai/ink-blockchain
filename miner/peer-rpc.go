a32935cdcdb1820181f29003349901500ef67ad4
jtache <j.andreitabs@gmail.com>
2018-02-09 12:30:46 -0800

    skeleton code for miner peer listener

---
+++ miner/peer-rpc.go
@@ -0,0 +1,100 @@
+/*
+
+This file contains the following:
+1. RPC definitions for miner peer to peer communication
+2. Structs for request and reply for the above
+3. Function to initialize the miner peer listener
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+	"os"
+)
+
+/*******************
+* TYPE_DEFINITIONS *
+*******************/
+
+type peerRpc struct {
+	miner *Miner
+}
+
+type empty struct{}
+
+type connectArgs struct {
+	peer_addr string
+}
+
+type propagateOpArgs struct {
+	op empty // TODO: proper struct here
+}
+
+type propagateBlockArgs struct {
+	block empty // TODO: proper struct here
+}
+
+type getBlockChainArgs struct {
+	blockChain empty // TODO: proper struct here
+}
+
+/***********************
+* FUNCTION_DEFINITIONS *
+***********************/
+
+// Adds the connecting peer to the list of maintained peers. The peer
+// requesting connect will be added to the maintained peer count. There will
+// be a heartbeat procedure for it, and any data propagations will be sent to
+// the peer as well.
+func (p *peerRpc) Connect(args *connectArgs, reply *empty) error {
+	fmt.Println("Connect called")
+	return nil
+}
+
+// This RPC is a no-op. It's used by the peer to ensure that this miner is
+// still alive.
+func (p *peerRpc) Hb(args *empty, reply *empty) error {
+	fmt.Println("Hb called")
+	return nil
+}
+
+// This RPC is used to send an operation (addshape, deleteshape) to miners.
+// Will not return any useful information.
+func (p *peerRpc) PropagateOp(args *propagateOpArgs, reply *empty) error {
+	fmt.Println("PropagateOp called")
+	return nil
+}
+
+// This RPC is used to send a new block (addshape, deleteshape) to miners.
+// Will not return any useful information.
+func (p *peerRpc) PropagateBlock(args *propagateBlockArgs, reply *empty) error {
+	fmt.Println("PropagateBlock called")
+	return nil
+}
+
+// This RPC is used for peers to get latest information when they have newly
+// initalized. No useful argument.
+func (p *peerRpc) GetBlockChain(args *empty, reply *getBlockChainArgs) error {
+	fmt.Println("GetBlockChain called")
+	return nil
+}
+
+// This will initialize a miner peer listener.
+func listenPeerRpc(addr string, miner *Miner) {
+	pRpc := peerRpc{miner}
+
+	conn, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("Peer RPC could not initialize tcp listener")
+		os.Exit(1)
+	}
+
+	server := rpc.NewServer()
+	server.RegisterName("Peer", pRpc)
+
+	server.Accept(conn)
+}
