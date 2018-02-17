69f3e2b2c877ea096e1fb6da6f9205e380dfcd05
Ubuntu <g6y9a@miner1.bt0cyxrw1hxezgsx2sss3qvpeh.xx.internal.cloudapp.net>
2018-02-17 07:55:35 +0000

    top level inkminer works now

--- ink-miner.go
+++ ink-miner.go
@@ -2,7 +2,7 @@
 package main
 
 import "./miner"
-import "ioutil"
+import "io/ioutil"
 import "os"
 import "strings"
 
@@ -10,11 +10,10 @@ func main(){
 	serverIP := os.Args[1]
 	
 	// Grab pubKey and privKey from key-pairs.txt
-	keyBytes, err := ioutil.ReadFile("./key-pairs.txt")
-	_ = CheckError(err, "main:ioutil.ReadFile")
+	keyBytes, _ := ioutil.ReadFile("./key-pairs.txt")
 	keyString := string(keyBytes[:])
-	privKey = strings.Split(keyString, "\n")[0]
-	pubKey = strings.Split(keyString, "\n")[1]
+	privKey := strings.Split(keyString, "\n")[0]
+	pubKey := strings.Split(keyString, "\n")[1]
 
 	miner.Mine(serverIP, pubKey, privKey)
 }
